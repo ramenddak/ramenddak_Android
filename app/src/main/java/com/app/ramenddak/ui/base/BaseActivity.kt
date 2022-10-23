fc2fcb59b9a4aa5db2e4cfaae44b0ab1f2c07f30
khs3994 <z01039948003@gmail.com>
2022-10-23 23:47:19 +0900

    :sparkles: :: start ui

--- app/src/main/java/com/app/ramenddak/ui/base/BaseActivity.kt
+++ app/src/main/java/com/app/ramenddak/ui/base/BaseActivity.kt
@@ -21,13 +21,6 @@ abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes private val layoutRe
 
     abstract fun init()
 
-    override fun onBackPressed() {
-        if (System.currentTimeMillis() - waitTime >= 1500) {
-            waitTime = System.currentTimeMillis()
-            Toast.makeText(this, "뒤로가기 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
-        } else finish()
-    }
-
     protected fun shortShowToast(msg: String) =
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
 
