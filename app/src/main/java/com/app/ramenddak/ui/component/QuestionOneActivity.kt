32bb629faa7ea5f351159b1cc51a34b590276900
khs3994 <z01039948003@gmail.com>
2022-11-24 15:11:38 +0900

    첫번째 질문 페이지 퍼블리싱

---
+++ app/src/main/java/com/app/ramenddak/ui/component/QuestionOneActivity.kt
@@ -0,0 +1,11 @@
+package com.app.ramenddak.ui.component
+
+import com.app.ramenddak.R
+import com.app.ramenddak.databinding.ActivityMainBinding
+import com.example.presentation.base.BaseActivity
+
+class QuestionOneActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun init() {
+
+    }
+}
--- app/src/main/java/com/app/ramenddak/ui/component/StartActivity.kt
+++ app/src/main/java/com/app/ramenddak/ui/component/StartActivity.kt
@@ -1,11 +1,14 @@
 package com.app.ramenddak.ui.component
 
+import android.content.Intent
 import com.app.ramenddak.R
 import com.app.ramenddak.databinding.ActivityStartBinding
 import com.example.presentation.base.BaseActivity
 
 class StartActivity : BaseActivity<ActivityStartBinding>(R.layout.activity_start) {
     override fun init() {
-
+        binding.goMain.setOnClickListener {
+            startActivity(Intent(this, QuestionOneActivity::class.java))
+        }
     }
 }
