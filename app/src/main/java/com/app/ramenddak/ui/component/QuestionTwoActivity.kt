943c7ccbe107243adf588bbaa4e5222fb3965694
khs3994 <z01039948003@gmail.com>
2022-11-24 16:02:37 +0900

    두번째 질문 페이지 퍼블리싱

---
+++ app/src/main/java/com/app/ramenddak/ui/component/QuestionTwoActivity.kt
@@ -0,0 +1,12 @@
+package com.app.ramenddak.ui.component
+
+import com.app.ramenddak.R
+import com.app.ramenddak.databinding.ActivityQuestionTwoBinding
+import com.example.presentation.base.BaseActivity
+
+class QuestionTwoActivity :
+    BaseActivity<ActivityQuestionTwoBinding>(R.layout.activity_question_two) {
+    override fun init() {
+
+    }
+}
